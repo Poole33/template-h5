aa84a57ad8ca87d94a1bc44d8e4068d868c71efc
Poole <ptcp3@hotmail.com>
2026-07-16 15:29:20 +0800

    chore(config): 整理项目入口文件和主脚本的代码格式与导入顺序
    - 优化index.html的标签格式，修正DOCTYPE大小写、meta和link标签写法，调整web app配置的meta属性
    - 调整main.js的导入语句顺序，优化vConsole初始化的代码结构

--- src/main.js
+++ src/main.js
@@ -1,23 +1,26 @@
-import '@unocss/reset/normalize.css'
-import '@/assets/styles/main.css'
-import './permission.js'
+import { createPinia } from 'pinia'
+import TDesign from 'tdesign-mobile-vue'
+import VConsole from 'vconsole'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia'
+import '@unocss/reset/normalize.css'
+import '@/assets/styles/main.css'
+import './permission.js'
 import 'virtual:uno.css'
 import 'animate.css'
-import TDesign from 'tdesign-mobile-vue'
+
 import 'tdesign-mobile-vue/es/style/index.css'
 
-import VConsole from 'vconsole'
-let vConsole = undefined
+let vConsole
 // vConsole = new VConsole()
 if (import.meta.env.DEV) {
   vConsole = new VConsole()
-} else vConsole?.destroy()
-
+}
+else {
+  vConsole?.destroy()
+}
 
 const app = createApp(App)
 const pinia = createPinia()
